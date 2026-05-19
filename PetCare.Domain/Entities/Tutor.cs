1cf45f962f0ca5133815fedcfd6ef0698bc4679b
LucasRafa22 <lucassolimene22@gmail.com>
2026-05-19 09:46:38 -0300

    feature: arrumando classes

--- PetCare.Domain/Entities/Tutor.cs
+++ PetCare.Domain/Entities/Tutor.cs
@@ -3,9 +3,12 @@ namespace PetCare.Domain.Entities;
 public class Tutor
 {
     public int IdTutor { get; set; }
-    public string Nome { get; set; }
-    public string Telefone { get; set; }
-    public string Email { get; set; }
 
-    public List<Pet> Pets { get; set; }
+    public string Nome { get; set; } = string.Empty;
+
+    public string? Telefone { get; set; }
+
+    public string? Email { get; set; }
+
+    public ICollection<Pet> Pets { get; set; } = new List<Pet>();
 }
